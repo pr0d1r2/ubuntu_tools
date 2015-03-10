691513bdc2d13ae220f84fba089f693edb8a2265
Marcin Nowicki <pr0d1r2@gmail.com>
2015-03-10 17:09:14 +0100

    Add script that allow to chage resolution in virtualbox.

---
+++ allow_to_change_resolution_on_virtualbox.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# source: http://askubuntu.com/questions/452108/cannot-change-screen-size-from-640x480-after-14-04-installation-on-virtualbox-os
+
+sudo apt-get install virtualbox-guest-dkms
